7271afb6d1a5408074258c5248e420fa5146f65a
Mustafa Elsharawy <memomeme621@gmail.com>
2023-08-28 18:39:41 +0300

    Algortithms

---
+++ Algorithms/MainTemplateSnippet.js
@@ -0,0 +1,32 @@
+{
+	"Main Template ":
+	{
+		// "scope": "javascript,typescript",
+		"prefix": "MeMo",
+		"body": [
+			"/*",
+			"\tauthor: Mustafa Elsharawy",
+			"\t@Date: ${CURRENT_YEAR}-${CURRENT_MONTH}-${CURRENT_DATE}",
+			"*/\n",
+			"#include <bits/stdc++.h>",
+			"using namespace std;\n",
+			"#define ll long long",
+			"#define fast ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);\n",
+			"constexpr int N = 1e5 + 5;",
+			"constexpr int mod = 1e9 + 7;\n",
+			"int main()\n{",
+			"\tfast",
+			"\t#ifndef ONLINE_JUDGE",
+			"\tfreopen(\"input.txt\", \"r\", stdin);",
+			"\tfreopen(\"output.txt\", \"w\", stdout);",
+			"\t#endif",
+			"\tint t = 1;",
+			"\tcin >> t;",
+			"\twhile (t--)\n\t{\n",
+			"\t}",
+			"\treturn 0;",
+			"}"
+		],
+		"description": "Print My Main Template"
+	}
+}
